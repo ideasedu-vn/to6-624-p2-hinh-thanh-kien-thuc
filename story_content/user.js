cb3a48836cdc3cd589b29a42586de9cc1881b657
anhdvt <anhdvt@ideasedu.vn>
2023-02-10 15:08:39 +0700

    upd

---
+++ story_content/user.js
@@ -0,0 +1,73 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "64gyJVOhofm":
+        Script1();
+        break;
+      case "6rgfuutI1PG":
+        Script2();
+        break;
+      case "5VyylOznusv":
+        Script3();
+        break;
+      case "5erXehlOb1B":
+        Script4();
+        break;
+      case "5vV9zmGGW4S":
+        Script5();
+        break;
+      case "5w4hrAZ4soS":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var e1 = player.GetVar('ô111');
+var e2 = e1.replace(/\s+/g, ''); 
+player.SetVar('ô111',e2);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var e3 = player.GetVar('ô222');
+var e4 = e3.replace(/\s+/g, ''); 
+player.SetVar('ô222',e4);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+var e1 = player.GetVar('oo1');
+var e2 = e1.replace(/\s+/g, ''); 
+player.SetVar('oo1',e2);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+var e3 = player.GetVar('oo2');
+var e4 = e1.replace(/\s+/g, ''); 
+player.SetVar('oo2',e4);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+var e5 = player.GetVar('oo3');
+var e6 = e1.replace(/\s+/g, ''); 
+player.SetVar('oo3',e6);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+var e7 = player.GetVar('oo4');
+var e8 = e1.replace(/\s+/g, ''); 
+player.SetVar('oo4',e8);
+}
+
